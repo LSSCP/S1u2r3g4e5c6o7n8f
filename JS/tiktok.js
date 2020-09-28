0c1cc02df36859a7d63e525670906a98ad9107f8
LSSCP <21199343+LSSCP@users.noreply.github.com>
2020-09-29 07:18:31 +0800

    Create tiktok.js

---
+++ JS/tiktok.js
@@ -0,0 +1,17 @@
+//rewrite"
+//https?:\/\/.+\.(tiktokv|byteoversea|ipstatp|tiktokcdn|ibytedtos|sgsnssdk)\.com\/.+ url script-request-header https://raw.gith
+
+
+
+function replaceQueryString (str) {
+    const re1 = new RegExp('(?<=&mcc_mnc=)46002','gi')
+    const re2 = new RegExp('(?<=&sim_region=)CN','gi')
+    const re3 = new RegExp('(?<=&(access|ac)=)4g', 'gi')
+    const re4 = new RegExp('(?<=carrier_region=)CN','gi')
+    return str.replace(re1,'').replace(re2,'').replace(re3,'mobile').replace(re4,'JP')
+}
+
+var req = $request.url;
+modifiedurl=replaceQueryString(req)
+
+$done({url: modifiedurl});
